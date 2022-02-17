bc793ab8437a6ab17e169c0f79635cebcfd3e548
Douglas Nelson <douganddeb3@msn.com>
2022-02-17 16:37:45 +0000

    update features

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function compute()
 {
     
     var principal = parseFloat(document.getElementById("principal").value);
-    if(principal<= 0){
+    if(principal<= 0 || principal==""){
         alert("Amount must be greater than 0.");
         document.getElementById("principal").focus();
     }
@@ -11,11 +11,14 @@ function compute()
     var interest = principal * years * parseFloat(rate) /100;
     var year = new Date().getFullYear()+parseInt(years);
     var result= document.getElementById("result");
+    var balance=principal + interest;
     result.innerHTML=`If you deposit ${principal},<br/>
-    at an interest rate of ${rate}%,<br/>
-    you will receive an amount of ${interest.toFixed(2)}, <br/>
-    in the year ${year}.`;
-    result.style.color= 'red';
+    at an interest rate of <span class="number">${rate}%</span>,<br/>
+    you will receive an amount of $<span class="number">${interest.toFixed(2)}</span>, <br/>
+    in the year ${year}.<br/>
+    Your balance in <span class="number">${year}</span><br/>
+    will be $<span class="number">${balance}.</span>`;
+    
 }
 
 function updateRate() 
