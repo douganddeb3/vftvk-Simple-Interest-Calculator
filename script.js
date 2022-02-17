ab00d606630e4d61a9a8824bd1d1ce00f3c8e1ab
Douglas Nelson <douganddeb3@msn.com>
2022-02-17 14:06:45 +0000

    Final Project done

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
     
+    var principal = parseFloat(document.getElementById("principal").value);
+    if(principal<= 0){
+        alert("Amount must be greater than 0.");
+        document.getElementById("principal").focus();
+    }
+    var rate = document.getElementById("rate").value;
+    var years = parseInt(document.getElementById("years").value);
+    var interest = principal * years * parseFloat(rate) /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var result= document.getElementById("result");
+    result.innerHTML=`If you deposit ${principal},<br/>
+    at an interest rate of ${rate}%,<br/>
+    you will receive an amount of ${interest.toFixed(2)}, <br/>
+    in the year ${year}.`;
+    result.style.color= 'red';
+}
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText= `${rateval}%`;
 }
         
